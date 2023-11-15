74736bd4086a73a32c78bddd6dd33fc4de429581
kukuSiM <vanusha.ivchenko@gmail.com>
2023-11-15 18:43:31 +0300

    ывап

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const path = require("node:path");
 
 const app = new Koa();
 
+// Middleware для установки правильного Content-Type для JavaScript файлов
+app.use(async (ctx, next) => {
+  if (ctx.path.endsWith('.js')) {
+    ctx.type = 'application/javascript';
+  }
+  await next();
+});
+
+// Сначала установите middleware для JavaScript, затем static middleware
 app.use(static(path.join(__dirname, "/")));
 
 app.listen(8080, () => {
@@ -16,3 +25,4 @@ app.listen(8080, () => {
                     http://localhost:8080
   `);
 });
+
