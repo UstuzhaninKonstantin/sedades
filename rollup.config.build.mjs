70487148f936f6d3369daefb13dcaaf73c2dd218
kukuSiM <vanusha.ivchenko@gmail.com>
2023-11-15 18:02:11 +0300

    ебучее изменение

--- rollup.config.build.mjs
+++ rollup.config.build.mjs
@@ -2,6 +2,14 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
+// Middleware для установки правильного Content-Type для JavaScript файлов
+app.use(async (ctx, next) => {
+  if (ctx.path.endsWith('.js')) {
+    ctx.type = 'application/javascript';
+  }
+  await next();
+});
+
 export default {
   input: "./src/index.ts",
   output: {
